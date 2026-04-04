862052a7bf5bb4a795a42b3bf5982055f2630772
laptop <info@datablox.co.za>
2026-04-04 14:35:11 +0200

    changes to deployment.py

--- routes/deployment.py
+++ routes/deployment.py
@@ -335,15 +335,6 @@ module.exports = nextConfig;"""
     listen 80;
     server_name {domain} www.{domain};
 
-    location /api/ {{
-        proxy_pass http://127.0.0.1:5000;
-        proxy_http_version 1.1;
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto $scheme;
-    }}
-
     location / {{
         proxy_pass http://localhost:{allocated_port};
         proxy_http_version 1.1;
